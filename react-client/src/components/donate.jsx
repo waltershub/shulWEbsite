2bd0fe55e9cf799078e1e18f2477fc27adba1380
waltershub <waltershub@gmail.com>
2017-12-19 16:07:06 -0500

    changed donate button

--- react-client/src/components/donate.jsx
+++ react-client/src/components/donate.jsx
@@ -5,8 +5,8 @@ const Donate = () => (
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
       <input type="hidden" name="cmd" value="_s-xclick" />
       <input type="hidden" name="hosted_button_id" value="EHJHCEA69DZNJ" />
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-      <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" />
+      <input type="image" src="/assets/images/slideshow/donate.png" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+      <img className="donateImg" alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" />
     </form>
   </div>
 );
