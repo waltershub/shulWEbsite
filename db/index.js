b091c9ad71ab2adf8cbdba01e1041c05e687fbec
waltershub <waltershub@gmail.com>
2017-12-27 00:52:43 -0500

    preparing for deploy

--- db/index.js
+++ db/index.js
@@ -58,7 +58,7 @@ const client = s3.createClient({
   s3RetryDelay: 1000, // this is the default
   multipartUploadThreshold: 20971520, // this is the default (20 MB)
   multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: config.aws,
+  s3Options: { accessKeyId: process.env.accessKeyId, secretAccessKey: process.env.secretAccessKey, region: process.env.region },
 });
 
 
